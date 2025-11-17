7f8ee2a37ce233e9e8804423a645b863020ccba7
Ghostyaq <mitchellhung176@gmail.com>
2025-11-16 20:02:27 -0500

    yes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-/** @type {import('.tailwindcss/vite').Config} */
+/** @type {import('tailwindcss').Config} */
 export default {
     content: ["./index.html", "./src/**/*.{html,ts}"],
     theme: { extend: {} },
